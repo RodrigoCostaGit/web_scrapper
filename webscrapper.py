759256b2c692801300a1ae796b91fd93dcf5c379
RodrigoOlman <58253035+RodrigoOlman@users.noreply.github.com>
2019-12-01 21:17:11 +0000

    Update webscrapper.py
    movie name

--- webscrapper.py
+++ webscrapper.py
@@ -7,11 +7,15 @@ soup=BeautifulSoup(data.text,"lxml")
 # print(soup.prettify())
 # top250=soup.find_all("tbody",class_="lister-list")
 # top250=soup.find_all("tr")
+
 titulo=[]
 score=[]
 top250=soup.tbody.find_all("tr")
 print(len(top250))
 for movies in top250:
-    vari=movies.find("td",class_="titleColumn").text
-    titulo.append(vari)
+    vari=movies.find("td",class_="titleColumn")
+    vari2=vari.find("a",title_="").text
+    # vari=movies.find("a",href_="img alt")
+    titulo.append(vari2)
 print(titulo)
+
