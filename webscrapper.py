d341926183a10f1b0409432a692c31350ebe9fe5
RodrigoOlman <58253035+RodrigoOlman@users.noreply.github.com>
2019-12-02 17:51:47 +0000

    2
    panda missing. rest is done

--- webscrapper.py
+++ webscrapper.py
@@ -4,18 +4,26 @@ import requests
 url="https://www.imdb.com/chart/top/"
 data=requests.get(url)
 soup=BeautifulSoup(data.text,"lxml") 
-# print(soup.prettify())
-# top250=soup.find_all("tbody",class_="lister-list")
-# top250=soup.find_all("tr")
+
 
 titulo=[]
 score=[]
+year=[]
 top250=soup.tbody.find_all("tr")
 print(len(top250))
 for movies in top250:
-    vari=movies.find("td",class_="titleColumn")
-    vari2=vari.find("a",title_="").text
-    # vari=movies.find("a",href_="img alt")
-    titulo.append(vari2)
-print(titulo)
+    #fetches the name of the movie
+    nomes=movies.find("td",class_="titleColumn")
+    nomes2=nomes.find("a",title_="").text
+    titulo.append(nomes2)
+#fetches the score of the movie
+    rating=movies.find("strong").text
+    score.append(rating)
+#fetches the year of the movie
+    movie_year=movies.find("span",class_="secondaryInfo").text
+    year.append(movie_year)
 
+
+print(titulo)
+print(score)
+print(year)
